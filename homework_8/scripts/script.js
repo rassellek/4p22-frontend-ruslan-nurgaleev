e7be386238ce4b191ed260f2d72b3ea2e17f134a
rassellek <rassellek@bk.ru>
2022-10-28 16:35:08 +0500

    edit homework_8

--- homework_8/scripts/script.js
+++ homework_8/scripts/script.js
@@ -1,35 +1,66 @@
-'use strict'
+"use strict";
 
-const button = document.getElementById('submit');
-const output = document.getElementById('output');
+const button = document.getElementById("submit");
+const output = document.getElementById("output");
 
-button.addEventListener('click', (event) => {
-    let firstNumber = document.getElementById('first-number').value;
-    const operation = document.getElementById('operation').value;
-    let secondNumber = document.getElementById('second-number').value;
-    let result;
+button.addEventListener("click", (event) => {
+  let firstNumber = document.getElementById("first-number").value;
+  const operation = document.getElementById("operation").value;
+  let secondNumber = document.getElementById("second-number").value;
+  let result;
 
-    event.preventDefault();
+  event.preventDefault();
 
-    if ((firstNumber == '') || (String(firstNumber).trim() === '')) 
-    { console.log('Первое число не указано'); return output.innerText = 'Первое число не указано'; }
+  if (firstNumber == "" || String(firstNumber).trim() === "") {
+    console.log("Первое число не указано");
+    return (output.innerText = "Первое число не указано");
+  }
 
-    if ((secondNumber == '') || (String(secondNumber).trim() === '')) 
-    { console.log('Второе число не указано'); return output.innerText = 'Второе число не указано'; }
-   
-    if (isNaN(firstNumber) || isNaN(secondNumber)) { console.log('Некорректный ввод чисел'); return output.innerText = 'Некорректный ввод чисел'; }
+  if (secondNumber == "" || String(secondNumber).trim() === "") {
+    console.log("Второе число не указано");
+    return (output.innerText = "Второе число не указано");
+  }
 
-    switch (operation) {
-        case '': { console.log('Не введён знак'); return output.innerText = 'Не введён знак'; }
-        case '+': { result = Number(firstNumber) + Number(secondNumber); break; }
-        case '-': { result = Number(firstNumber) - Number(secondNumber); break; }
-        case '*': { result = Number(firstNumber) * Number(secondNumber); break; }
-        case '/': { result = Number(firstNumber) / Number(secondNumber); break; }
-        default: { console.log('Программа не поддерживает такую операцию'); return output.innerText = 'Программа не поддерживает такую операцию'; }
-    };
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    console.log("Некорректный ввод чисел");
+    return (output.innerText = "Некорректный ввод чисел");
+  }
 
-    if ((output !== 'Программа не поддерживает такую операцию') && (isNaN(result) || result === Infinity)) {
-        console.log('Операция некорректна'); return output.innerText = 'Операция некорректна';
-    } else { console.log(result); return output.innerText = `Результат: ${result}` }
+  switch (operation) {
+    case "": {
+      console.log("Не введён знак");
+      return (output.innerText = "Не введён знак");
+    }
+    case "+": {
+      result = Number(firstNumber) + Number(secondNumber);
+      break;
+    }
+    case "-": {
+      result = Number(firstNumber) - Number(secondNumber);
+      break;
+    }
+    case "*": {
+      result = Number(firstNumber) * Number(secondNumber);
+      break;
+    }
+    case "/": {
+      result = Number(firstNumber) / Number(secondNumber);
+      break;
+    }
+    default: {
+      console.log("Программа не поддерживает такую операцию");
+      return (output.innerText = "Программа не поддерживает такую операцию");
+    }
+  }
 
+  if (
+    output !== "Программа не поддерживает такую операцию" &&
+    (isNaN(result) || result === Infinity)
+  ) {
+    console.log("Операция некорректна");
+    return (output.innerText = "Операция некорректна");
+  } else {
+    console.log(result);
+    return (output.innerText = `Результат: ${result}`);
+  }
 });
